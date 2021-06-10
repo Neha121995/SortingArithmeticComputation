0e05f67c96f2850b10c549682a69df9065a42648
Neha121995 <jagtapneha1212@gmail.com>
2021-06-10 12:43:47 +0530

    Add a function to store array in descending order

---
+++ storeResultInDecendingOrder.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+declare -A Arithmetic
+echo "Welcome to Sorting Arithmetic Computation Problem"
+# input take from user
+read -p "Enter A Number :" a
+read -p "Enter B Number :" b
+read -p "Enter C Number :" c
+
+result1=$(($a+$b*$c))
+
+result2=$(($a*$b+$c))
+
+result3=$(($c+$a/$b))
+
+result4=$(($a%$b+$c))
+
+
+Arithmetic[CP1]="$result1"
+Arithmetic[CP2]="$result2"
+Arithmetic[CP3]="$result3"
+Arithmetic[CP4]="$result4"
+
+echo "All Arithmetic Computations And their Result in Dictionary :"
+
+echo "All Arithmetic Computations Result : ${Arithmetic[@]}"
+echo "Arithmetic Computation Program Is : ${!Arithmetic[@]}"
+
+
+for (( i=1; i<=${#Arithmetic[@]}; i++ ))
+do
+	echo "CP$i : ${Arithmetic[CP$i]}"
+done
+
+echo "All Arithmetic Computations And their Result in Array :"
+for (( i=1; i<=${#Arithmetic[@]}; i++ ))
+do
+        array[$i]=${Arithmetic[CP$i]}
+done
+echo "${array[@]}"
+
+n=${#array[@]}
+
+for (( x=$n; x<=1; x-- ))
+do
+        for (( y=$n-1; y<=1; y-- ))
+        do
+                if [ ${array[y]} -gt ${array[x]} ]
+                then
+                        temp=${array[x]}
+                        array[$x]=${array[y]}
+                        array[$y]=$temp
+                fi
+        done
+done
+
+echo "Sorted array in Descending Order : ${array[@]}"
