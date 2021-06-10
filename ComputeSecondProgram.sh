cb1d4802d54aae9ef90992e763eab7a84447ed65
Neha121995 <jagtapneha1212@gmail.com>
2021-06-10 11:51:09 +0530

    Add a function to get result of second computation problem

---
+++ ComputeSecondProgram.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+echo "Welcome to Sorting Arithmetic Computation Problem"
+# input take from user
+read -p "Enter First Number :" a
+read -p "Enter Second Number :" b
+read -p "Enter Third Number :" c
+
+result1=`awk "BEGIN {print $a + $b * $c}"`
+echo " Result of First Computation Program is : $result1"
+
+result2=`awk "BEGIN {print $a * $b + $c}"`
+echo " Result of Second Computation Program is : $result2"
