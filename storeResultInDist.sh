879de0dba6ddf04e86be47a113e7dd62cbce5a2d
Neha121995 <jagtapneha1212@gmail.com>
2021-06-10 12:26:49 +0530

    Add a function to store the computations result in a dictionary

---
+++ storeResultInDist.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+declare -A Arithmetic
+echo "Welcome to Sorting Arithmetic Computation Problem"
+# input take from user
+read -p "Enter A Number :" a
+read -p "Enter B Number :" b
+read -p "Enter C Number :" c
+
+result1=`awk "BEGIN {print $a + $b * $c}"`
+
+result2=`awk "BEGIN {print $a * $b + $c}"`
+
+result3=`awk "BEGIN {print $c + $a / $b}"`
+
+result4=`awk "BEGIN {print $a % $b + $c}"`
+
+
+Arithmetic[CP1]="$result1"
+Arithmetic[CP2]="$result2"
+Arithmetic[CP3]="$result3"
+Arithmetic[CP4]="$result4"
+
+echo "All Arithmetic Computations Result : ${Arithmetic[@]}"
+echo "Arithmetic Computation Program Is : ${!Arithmetic[@]}"
+echo "All Arithmetic Computations And their Result"
+
+for (( i=1; i<=${#Arithmetic[@]}; i++ ))
+do
+	echo "CP$i : ${Arithmetic[CP$i]}"
+done
